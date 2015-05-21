19a6c0e7f7d9063ce9120d5c575ca614014b5ff3
jdhakar <jnagar1729@gmail.com>
2015-05-21 12:38:21 +0530

    changes in graphene.cabal to update base version and imports

--- Graphene.hs
+++ Graphene.hs
@@ -1,6 +1,8 @@
 -- | The top level module.
 module Graphene
-       ( module Graphene.Types
+       ( module Graphene.Types,
+         module Graphene.ForceLayout
        ) where
 
 import Graphene.Types
+import Graphene.ForceLayout
